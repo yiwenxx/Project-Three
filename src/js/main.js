d5ff6c141b6b39975ccfc5b3fe30c7ab783fa6da
Yiwen Xu <yiwenx119@gmail.com>
2022-04-26 21:05:38 -0400

    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
+// Parallax
+$(".parallax-section").parallax({
+  speed: 0.1,
+});
 
+/* Card Effect */
+
+var CardFlip = document.querySelectorAll(".card");
+
+for (var card of CardFlip) {
+  card.addEventListener("click", Flip);
+}
+
+function Flip() {
+  this.classList.toggle("is-flipped");
+}
