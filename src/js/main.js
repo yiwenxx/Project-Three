7c99fcd65e0a5e2cb3aae41582cfe0aa88640776
Yiwen Xu <yiwenx119@gmail.com>
2022-04-27 20:32:19 -0400

    Final Version

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
-// Parallax
-$(".parallax-section").parallax({
-  speed: 0.1,
-});
+/* Set up */
 
 /* Card Effect */
 
@@ -13,4 +10,60 @@ for (var card of CardFlip) {
 
 function Flip() {
   this.classList.toggle("is-flipped");
+}
+
+/* Slider */
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
+
+/* Meme */
+
+const imageArray = [
+  "1.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+  "8.jpeg",
+  "9.jpeg",
+  "10.jpeg",
+  "11.jpeg",
+  "12.jpeg",
+  "13.jpeg",
+  "14.jpeg",
+  "15.jpeg",
+  "16.jpeg",
+  "17.jpeg",
+  "18.jpeg",
+];
+
+function getRandomImage() {
+  random_index = Math.floor(Math.random() * imageArray.length);
+  selected_image = imageArray[random_index];
+  document.getElementById("image_shower").src = `../img/meme/${selected_image}`;
+}
+
+getRandomImage();
+
+/* Source */
+const accordionItemHeaders = document.querySelector(".source");
+
+accordionItemHeaders.addEventListener("click", Source);
+
+function Source() {
+  accordionItemHeaders.classList.toggle("active");
+  const accordionItemBody = accordionItemHeaders.nextElementSibling;
+  if (accordionItemHeaders.classList.contains("active")) {
+    accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+  } else {
+    accordionItemBody.style.maxHeight = 0;
+  }
 }
